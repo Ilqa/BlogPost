c6d780a25908847c7c6f641523b7cafcfe1f06dd
ilqa <ilqanawaz@gmail.com>
2020-07-27 01:40:11 +0500

    changes

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,12 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+def aboutme(request):
+    return render(request, 'blog/about.html')
+
+def why_blog(request):
+    return render(request, 'blog/whyblog.html')
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
